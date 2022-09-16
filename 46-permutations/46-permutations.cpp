aef07d63c47f3cfa0f2d3cf73daa9994ad0a481c
Ashik Khan <aasshhik98@gmail.com>
2022-09-16 22:59:24 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (91.55%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    void next_permutation(vector<vector<int>> &results,vector<int> nums){
-        int nsize = nums.size()-1;
-        int missed = -1,sw = nsize;
-        
-        while(nsize>=1){
-            // cout<<nums[nsize]<<">"<< nums[nsize-1]<<endl;
-            if(nums[nsize] > nums[nsize-1]){missed = nsize-1; break;}
-            nsize--;
+    void comPermute(int index,vector<int> &nums,vector<vector<int>> &results){
+        if(index == nums.size()){
+            results.push_back(nums);
+            return;
         }
         
-        if(missed > -1){
-            for(int i=nums.size()-1; i>=0; i--){
-                if(nums[i] > nums[missed]){sw =i; break;}
-            }
-            // cout<<nums[missed]<<" "<<nums[sw]<<endl;
-            swap(nums[missed],nums[sw]);
-            reverse(nums.begin()+missed+1,nums.end());
-            results.push_back(nums);
-            next_permutation(results,nums);
-        }else{
-            return;
+        for(int i=index; i<nums.size(); i++){
+            swap(nums[index],nums[i]);
+            comPermute(index+1,nums,results);
+            swap(nums[index],nums[i]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> results;
-        sort(nums.begin(),nums.end());
-        results.push_back(nums);
-        next_permutation(results,nums);
+        
+        comPermute(0,nums,results);
+        
         return results;
     }
 };
