375139961065425279defeb6923eb3494f61d555
Ashik Khan <aasshhik98@gmail.com>
2022-09-17 21:09:29 +0530

    Time: 55 ms (40.04%), Space: 6.5 MB (16.37%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    
-    bool inValid(vector<vector<char>> &board,int row,int col,char c){
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c)
-              return false;
-
-            if (board[row][i] == c)
-              return false;
-
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
-              return false;
-          }
-          return true;
+    bool isValid(vector<vector<char>> &board,int row,int col,char c){
+        for(int i=0; i<9; i++){
+            if(board[row][i] == c){return false;}
+            if(board[i][col] == c){return false;}
+            
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3] == c){return false;}
+        }
+        
+        return true;
     }
     
-    bool solutionSoduko(vector<vector<char>>& board){
-        for(int i=0; i<board.size(); i++){
-            for(int j=0; j<board[i].size(); j++){
-                if(board[i][j] == '.'){
+    bool solutionSudoku(vector<vector<char>> &board){
+        for(int row=0; row<board.size(); row++){
+            for(int col=0; col<board[row].size(); col++){
+                if(board[row][col] == '.'){
                     for(char c='1'; c<='9'; c++){
-                        if(inValid(board,i,j,c) == true){
-                            board[i][j] = c;
-                        
-                            if(solutionSoduko(board))
+                        if(isValid(board,row,col,c)){
+                            board[row][col] = c;
+                            if(solutionSudoku(board))
                                 return true;
                             else
-                                board[i][j] = '.';
+                                board[row][col] = '.';
                         }
                     }
                     return false;
@@ -38,7 +33,6 @@ public:
     }
     
     void solveSudoku(vector<vector<char>>& board) {
-        
-        solutionSoduko(board);
+        solutionSudoku(board);
     }
 };
