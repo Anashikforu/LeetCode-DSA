07fd495ae10d7ca6bd9c81d1c4799af3316a0512
Ashik Khan <aasshhik98@gmail.com>
2022-09-18 22:33:00 +0530

    Time: 9 ms (33.37%), Space: 11.1 MB (29.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -10,28 +10,20 @@ public:
             
             if(nums[mid] == target){return mid;}
             
-            if (nums[low] <= nums[mid]) { //<---step 5
-              if (nums[low] <= target && nums[mid] >= target)
-                high = mid - 1; //<---step 6 
-              else
-                low = mid + 1; //<---step 7
-            } else { //<---step 7
-              if (nums[mid] <= target && target <= nums[high])
-                low = mid + 1;
-              else
-                high = mid - 1;
+            if (nums[low] <= nums[mid]) {
+                if(nums[low] <= target and nums[mid] >= target){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }else {
+                if(nums[mid] <= target and nums[high] >= target){
+                    low = mid+1;
+                }else{
+                    high = mid-1;
+                }
             }
             
-            // if(nums[mid] > nums[mid+1] and target <= last_val){
-            //     low = mid+1;
-            // }else if(mid > 0 and nums[mid-1] > nums[mid] and target > last_val){
-            //     cout<<low<<" "<<mid<<endl;
-            //     high = mid-1;
-            // }else if(((target < last_val) and (nums[mid] > last_val)) or(nums[mid] < target)){
-            //     low = mid+1;
-            // }else {
-            //     high = mid-1;
-            // }
         }
         
         return -1;
