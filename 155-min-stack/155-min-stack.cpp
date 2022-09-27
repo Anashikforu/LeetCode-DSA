ca135ade02d1e472b73e92547250c85ba5f31774
Ashik Khan <aasshhik98@gmail.com>
2022-09-27 15:41:25 +0530

    Time: 93 ms (5.20%), Space: 18.1 MB (5.22%) - LeetHub

---
+++ 155-min-stack/155-min-stack.cpp
@@ -0,0 +1,53 @@
+class MinStack {
+public:
+    
+    stack<int> mystack;
+    priority_queue<int, vector<int>, greater<int>> minQueue;
+    
+    MinStack() {
+        stack<int> temp;
+        priority_queue<int, vector<int>, greater<int>> mintemp;
+        mystack = temp;
+        minQueue = mintemp;
+    }
+    
+    void push(int val) {
+        mystack.push(val);
+        minQueue.push(val);
+    }
+    
+    void pop() {
+        priority_queue<int, vector<int>, greater<int>> temp;
+        
+        
+            while(mystack.top() != minQueue.top()){
+                temp.push(minQueue.top());
+                minQueue.pop();
+            }
+            minQueue.pop();
+            while(!temp.empty()){
+                minQueue.push(temp.top());
+                temp.pop();
+            }
+        
+        
+        mystack.pop();
+    }
+    
+    int top() {
+        return mystack.top();
+    }
+    
+    int getMin() {
+        return minQueue.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
