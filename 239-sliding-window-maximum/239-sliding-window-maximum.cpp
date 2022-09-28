d5460a5f65e5da9088569c70852cff4849dc8421
Ashik Khan <aasshhik98@gmail.com>
2022-09-29 01:39:21 +0530

    Time: 568 ms (43.99%), Space: 134.7 MB (81.62%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -4,13 +4,16 @@ public:
       deque < int > dq;
       vector < int > ans;
       for (int i = 0; i < nums.size(); i++) {
-        if (!dq.empty() && dq.front() == i - k) dq.pop_front();
-
-        while (!dq.empty() && nums[dq.back()] < nums[i])
-          dq.pop_back();
-
-        dq.push_back(i);
-        if (i >= k - 1) ans.push_back(nums[dq.front()]);
+          if(!dq.empty() and i-k>= dq.front()){
+              dq.pop_front();
+          }
+          
+          while(!dq.empty() and nums[i] > nums[dq.back()]){
+              dq.pop_back();
+          }
+          dq.push_back(i);
+          
+          if(i>=k-1){ans.push_back(nums[dq.front()]);}
       }
       return ans;
     }
