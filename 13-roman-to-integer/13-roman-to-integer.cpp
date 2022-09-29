39df3b92cb44374f56c40b9ba54b4709644dc424
Ashik Khan <aasshhik98@gmail.com>
2022-09-29 22:07:28 +0530

    Time: 24 ms (41.69%), Space: 6 MB (88.55%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        vector<int> values;
-        int result=0;
         
-        for(int i=s.length()-1; i>=0; i--){
-            if(s[i] == 'I'){    result += 1; }
-            else if(s[i] == 'V'){
-                if( i-1 < s.length() and s[i-1] == 'I'){ result -=2; }
-                result +=5; 
-            }
-            else if(s[i] == 'X'){
-                if( i-1 < s.length() and s[i-1] == 'I'){ result -=2; }
-                result +=10; 
-            }
-            else if(s[i] == 'L'){
-                if( i-1 < s.length() and s[i-1] == 'X'){ result -=20; }
-                result +=50; 
-            }
-            else if(s[i] == 'C'){
-                if( i-1 < s.length() and s[i-1] == 'X'){ result -=20; }
-                result +=100; 
-            }
-            else if(s[i] == 'D'){
-                if( i-1 < s.length() and s[i-1] == 'C'){ result -=200; }
-                result +=500; 
-            }
-            else if(s[i] == 'M'){
-                if( i-1 < s.length() and s[i-1] == 'C'){ result -=200; }
-                result +=1000; 
+        int result = 0;
+        
+        for(int i=0; i<s.length(); i++){
+            if(i < s.length()-1 and  s[i] == 'I' and s[i+1] == 'X'){
+                result += 9;
+                i++;
+            }else if(i < s.length()-1 and s[i] == 'I' and s[i+1] == 'V'){
+                result += 4;
+                i++;
+            }else if(s[i] == 'I'){
+                result += 1;
+            }else if(s[i] == 'V'){
+                result += 5;
+            }else if(i < s.length()-1 and s[i] == 'X' and s[i+1] == 'L'){
+                result += 40;
+                i++;
+            }else if(i < s.length()-1 and s[i] == 'X' and s[i+1] == 'C'){
+                result += 90;
+                i++;
+            }else if(s[i] == 'X'){
+                result += 10;
+            }else if(s[i] == 'L'){
+                result += 50;
+            }else if(i < s.length()-1 and s[i] == 'C' and s[i+1] == 'M'){
+                result += 900;
+                i++;
+            }else if(i < s.length()-1 and s[i] == 'C' and s[i+1] == 'D'){
+                result += 400;
+                i++;
+            }else if(s[i] == 'C'){
+                result += 100;
+            }else if(s[i] == 'D'){
+                result += 500;
+            }else if(s[i] == 'M'){
+                result += 1000;
             }
         }
-        
         
         return result;
     }
