6ac70074c31114ddddb604b39c2b64449459739a
Ashik Khan <aasshhik98@gmail.com>
2022-10-01 23:17:50 +0530

    Time: 3 ms (63.51%), Space: 6.2 MB (75.45%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+        vector<int> dp(n+1,0);
+        dp[n] = 1;
+        dp[n-1] = 1;
+        for(int i=n-2; i>=0; i--){
+            if(s[i+1] != '0')
+            dp[i] += dp[i+1];
+            if(s[i] != '0'){
+                string number = "";
+                number += s[i];
+                number += s[i+1];
+                int letter = stoi(number);
+                if(letter <= 26){
+                    if(s[i+2] != '0')
+                    dp[i] += dp[i+2];
+                }   
+            }
+        }
+        if(s[0] == '0')return 0;
+        
+        return dp[0];
+    }
+};
