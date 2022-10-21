011caaa85002b435f0445e8f281ae25e594c5bbb
Ashik Khan <aasshhik98@gmail.com>
2022-10-21 17:58:30 +0530

    Time: 48 ms (43.48%), Space: 13.8 MB (65.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
-    bool isCycle(int s,  vector < bool > &visited, vector < bool > &dfsvisited,vector < int > adj[]) {
-        visited[s] = 1;
-        dfsvisited[s] = 1;
-        for(auto a: adj[s]){
-            if(!visited[a]){
-                if(isCycle(a,visited,dfsvisited,adj)){return true;}
-            }else if(dfsvisited[a]){
-                return true;
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        vector<int> res;
+        vector<int> inDegree(numCourses,0);
+        for(auto a:prerequisites){
+            adj[a[1]].push_back(a[0]);
+            inDegree[a[0]]++;
+        }
+        
+        queue<int> pq;
+        for(int i=0; i<numCourses; i++){
+            if(inDegree[i] == 0){
+                pq.push(i);
+            }
+        }
+        
+        while(!pq.empty()){
+            int top = pq.front();
+            pq.pop();
+            res.push_back(top);
+            for(auto a:adj[top]){
+                --inDegree[a];
+                if(inDegree[a] == 0){
+                    pq.push(a);
+                }
             }
             
         }
-        dfsvisited[s] = 0;
-        return false;
-    }
-    
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
         
-        vector<int> adj[numCourses];
-        for(int i = 0; i<prerequisites.size(); ++i){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj[v].push_back(u);
-        }
-        
-        vector<bool> visited(numCourses,0);
-        vector<bool> dfsvisited(numCourses,0);
-        
-        for(int i=0; i<numCourses; i++){
-            if(!visited[i]){
-                if(isCycle(i,visited,dfsvisited,adj)){
-                    return false;
-                }
-            }
-        }
-        
-        return true;
+        return res.size()==numCourses?true:false;
     }
 };
