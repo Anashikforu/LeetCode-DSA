6630d32c7691c0b75599e17291be441f3e67ae2c
Ashik Khan <aasshhik98@gmail.com>
2022-11-17 13:31:10 +0530

    Time: 18 ms (35.34%), Space: 13.8 MB (25.91%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -5,11 +5,11 @@ public:
         int prod1= nums[0], prod2= nums[0], result =nums[0]; 
         
         for(int i=1; i<n; i++){
-            int temp = max({nums[i],prod1*nums[i],prod2*nums[i]});
-            prod2 = min({nums[i],prod1*nums[i],prod2*nums[i]});
+            int temp = max({prod2*nums[i],prod1*nums[i],nums[i]});
+            prod2 = min({prod2*nums[i],prod1*nums[i],nums[i]});
             prod1 = temp;
             
-            result =  max(result,prod1);
+            result = max(prod1,result);
         }
         
         return result;
