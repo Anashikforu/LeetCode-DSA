83d323414e59f74426ca1cd580e1574ed7fb3033
Ashik Khan <aasshhik98@gmail.com>
2022-11-24 10:01:57 +0530

    Time: 69 ms (36.75%), Space: 18.3 MB (54.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int amt;
-    
-    int solve(vector<int> &nums, int val, int index, vector<vector<int>> &dp)
-    {
-        if(val > amt) return 0;
-        if(val == amt) return 1;
+    int change(int amount, vector<int>& coins) {
+        int n= coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
         
-        if(dp[index][val] != -1) return dp[index][val];
-        
-        int sum = 0;
-        for(int i=index; i<nums.size(); i++)
-        {
-            sum += solve(nums, val+nums[i], i, dp);
+        for(int i=0; i<=amount; i++){
+            dp[0][i] = i%coins[0] == 0;
         }
         
-        return dp[index][val] = sum;
-    }
-    
-    int change(int amount, vector<int>& coins) {
+        for(int i=1; i<n; i++){
+            for(int j=0; j<=amount; j++){
+                dp[i][j] = dp[i-1][j];
+                if(coins[i] <= j){
+                    dp[i][j] += dp[i][j-coins[i]];
+                }
+            }
+        }
         
-        vector<vector<int>> dp(coins.size()+1, vector<int> (amount+5001, -1));
-        amt = amount;
-        return solve(coins, 0, 0, dp);
+        return dp[n-1][amount];
     }
 };
