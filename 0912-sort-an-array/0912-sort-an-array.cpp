dddfffc31cf73559697b8cc76cb1274700ca6dd6
Ashik Khan <aasshhik98@gmail.com>
2022-12-01 02:42:12 +0530

    Time: 472 ms (40.13%), Space: 57.4 MB (60.04%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,34 +1,47 @@
 class Solution {
 public:
-    void merge(vector<int>& nums,int left,int mid,int right){
-        int temp[right-left+1];
-        int i=left,j=mid+1,k=0;
-        while(i<=mid && j <= right){
-            if(nums[i]<nums[j]){temp[k++] = nums[i++];}
-            else{temp[k++] = nums[j++];}
+    
+    void merge(vector<int>& nums,int left,int mid,int right) {
+        
+        int i=left,j=mid,k=0;
+        int temp[right - left + 1];
+        
+        while(i<mid && j<=right){
+            if(nums[i] <= nums[j]){
+                temp[k++] = nums[i++];
+            }else{
+                temp[k++] = nums[j++];
+            }
         }
         
-        while(i<=mid){temp[k++] = nums[i++];}
-        while(j<=right){temp[k++] = nums[j++];}
+        while(i<mid){
+            temp[k++] = nums[i++];
+        }
+        
+        while(j<=right){
+            temp[k++] = nums[j++];
+        }
         
         for(int i=left; i<=right; i++){
             nums[i] = temp[i-left];
         }
+        
     }
     
-    void merge_sort(vector<int>& nums,int left,int right){
+    void merge_sort(vector<int>& nums,int left,int right) {
         int mid = (left+right)/2;
+        
         if(left<right){
             merge_sort(nums,left,mid);
             merge_sort(nums,mid+1,right);
-            merge(nums,left,mid,right);
+            
+            merge(nums,left,mid+1,right);
         }
+        
     }
     
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-        merge_sort(nums,0,n-1);
-        
+        merge_sort(nums,0,nums.size()-1);
         return nums;
     }
 };
