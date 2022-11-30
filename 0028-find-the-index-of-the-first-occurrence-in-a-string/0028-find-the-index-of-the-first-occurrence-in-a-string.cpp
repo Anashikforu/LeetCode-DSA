a0a173ea608a01dc973890a7b62c5f75ccaaca75
Ashik Khan <aasshhik98@gmail.com>
2022-11-30 18:43:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (29.94%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,15 +1,52 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(int i=0; i<haystack.size(); i++){
-            int flag =1;
-            for(int j=0; j<needle.size(); j++){
-                if(haystack[i+j] != needle[j]){
+        
+        int kfreq = 0,temp=0,i=0,j=needle.size()-1;
+        vector<int> hfreq(haystack.size(),0);
+        
+        for(auto a:needle){
+            kfreq += a - 'a' + 1;
+        }
+        
+        for(auto a:haystack){
+            temp += a - 'a' + 1;
+            hfreq[i] = temp;
+            i++;
+        }
+        
+        i=0;
+        int flag=1;
+        while(j<haystack.size()){
+            flag=1;
+            if(i == 0 ){
+                if(hfreq[j] == kfreq){
+                    for(int a=0; a<needle.size(); a++){
+                        if(haystack[a+i] != needle[a]){
+                            flag =0;
+                            break;
+                        }
+                    }
+                }else{
                     flag = 0;
-                    break;
                 }
+            }else if(hfreq[j] - hfreq[i-1] == kfreq){
+                for(int a=0; a<needle.size(); a++){
+                    if(haystack[a+i] != needle[a]){
+                        flag =0;
+                        break;
+                    }
+                }
+            }else{
+                flag = 0;
             }
-            if(flag == 1){return i;}
+            
+            if(flag == 1){
+                return i;
+            }
+            
+            i++;
+            j++;
         }
         
         return -1;
