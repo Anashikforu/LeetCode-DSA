a008db2149c2238e9e7e12896d2c6c7603bb54b5
Ashik Khan <aasshhik98@gmail.com>
2022-12-02 00:32:44 +0530

    Time: 48 ms (50.01%), Space: 14.7 MB (24.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool isCycle(int n, vector<int> adj[], vector<int>& visited){
-        if(visited[n] == 1)
-            return true;
-        
-        if(visited[n] == 0){
-            visited[n] = 1;
-            for(auto it:adj[n]){
-                if(isCycle(it,adj,visited))
-                    return true;
-            }
-        }
-        
-        visited[n] = 2;
-        
-        return false;
-    }
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> visited(numCourses,0);
-        
-        vector<int> adj[numCourses];
-        
-        for(auto edge:prerequisites){
-            adj[edge[1]].push_back(edge[0]);
+        vector<int> indegree(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+        for(auto row:prerequisites){
+            indegree[row[1]]++;
+            adj[row[0]].push_back(row[1]);
+            adj[row[1]].push_back(row[0]);
         }
         
+        queue<int> pq;
         for(int i=0; i<numCourses; i++){
-            if(isCycle(i,adj,visited)){
-                return false;
+            if(indegree[i]== 0){pq.push(i);}
+        }
+        
+        vector<int> res;
+        while(!pq.empty()){
+            int top = pq.front();
+            res.push_back(top);
+            pq.pop();
+            for(auto a:adj[top]){
+                indegree[a]--;
+                if(indegree[a] == 0){pq.push(a);}
             }
         }
         
-        return true;
+        return res.size()==numCourses;
     }
 };
