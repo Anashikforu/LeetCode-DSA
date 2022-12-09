a859bb512d87058ed924c8f8717d34e3aae9dd09
Ashik Khan <aasshhik98@gmail.com>
2022-12-10 02:45:26 +0530

    Time: 3 ms (67.79%), Space: 8.2 MB (71.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void swap(int *x,int *y){
-        int temp = *x;
-        *x = *y;
-        *y = temp;
-    } 
+    // void swap(int *x,int *y){
+    //     int temp = *x;
+    //     *x = *y;
+    //     *y = temp;
+    // } 
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         for(int i=0; i<n; i++){
             for(int j=0; j<n-i-1; j++){
                 if(nums[j] > nums[j+1]){
-                    swap(&nums[j],&nums[j+1]);
+                    swap(nums[j],nums[j+1]);
                 }
             }
         }
