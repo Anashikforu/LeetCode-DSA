bca08822e14335b7c33534a5ad25b4b521c2009f
Ashik Khan <aasshhik98@gmail.com>
2022-12-16 17:41:27 +0530

    Added solution - LeetHub

--- Sort The Array - GFG/sort-the-array.cpp
+++ Sort The Array - GFG/sort-the-array.cpp
@@ -5,30 +5,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int parittion(vector<int> &arr, int l,int h){
-        int pivot= arr[h];
-        int x=l;
+    int partition(vector<int> &arr,int l, int h){
+        int pivot = arr[h];
+        int x = l;
         
         for(int i=l; i<h; i++){
-            if(arr[i]<=pivot){
+            if(arr[i]<pivot){
                 swap(arr[i],arr[x]);
                 x++;
             }
         }
-        
         swap(arr[x],arr[h]);
-        
         return x;
     }
     
-    void quick_sort(vector<int> &arr, int l,int h){
+    void quick_sort(vector<int> &arr,int l, int h){
         if(l<h){
-            int pivot = parittion(arr,l,h);
+            int pivot = partition(arr,l,h);
             quick_sort(arr,l,pivot-1);
             quick_sort(arr,pivot+1,h);
         }
     }
-    
     vector<int> sortArr(vector<int>arr, int n){
     //complete the function here
         quick_sort(arr,0,n-1);
