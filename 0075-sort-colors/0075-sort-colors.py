185e23bb7de42b8c4248166bdf2458a9200e0b44
Ashik Khan <aasshhik98@gmail.com>
2023-03-07 00:42:42 +0530

    Time: 34 ms (74.14%), Space: 13.9 MB (9.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums[:] = sorted(nums)
+        color = {0: 0, 1: 0, 2: 0}
+        
+        for n in nums:
+            color[n] += 1
+        
+        ind =0
+        for c in color:
+            for col in range(color[c]):
+                nums[ind] = c
+                ind += 1
+            
+            
+        
         
