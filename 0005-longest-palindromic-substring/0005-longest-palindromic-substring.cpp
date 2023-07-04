8c43efa643fc9c54a7c5f8816c7d2290ef55757b
Ashik Khan <aasshhik98@gmail.com>
2023-07-04 16:21:55 +0530

    Time: 1480 ms (7.68%), Space: 6.9 MB (87.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -14,20 +14,15 @@ public:
     string longestPalindrome(string s) {
         
         int res = 0, x=0, y= 0;
-        vector<vector<int>> vect;
-
-	    vector<vector<int>> mat(s.length(),vector<int> (s.length(),0)) ;
+        
         
         for(int i=0; i<s.length(); i++){
             for(int j=0; j<i; j++){
-                if(mat[i][j] == 0){
-                    mat[i][j] = 1;
-                    if(palindrome(s,i,j)){
-                        if(i-j > res){
-                            res = i-j;
-                            y=i;
-                            x=j;
-                        }
+                if(palindrome(s,i,j)){
+                    if(i-j > res){
+                        res = i-j;
+                        y=i;
+                        x=j;
                     }
                 }
             }
