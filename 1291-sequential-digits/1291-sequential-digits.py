3039deeaeb94dc454fce3a1be7488044f26163eb
Ashik Khan <aasshhik98@gmail.com>
2024-02-20 03:59:43 +0600

    Time: 38 ms (30.27%), Space: 16.5 MB (44.23%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -5,13 +5,27 @@ class Solution:
         digits_low = len(str(low))
         digits_high = len(str(high))
         superset = "123456789"
-
-        for i in range(digits_low, digits_high + 1):
-            for j in range(0, 9 - i + 1):
-                sub_string = superset[j:j + i]
-                num = int(sub_string)
-                if low <= num <= high:
-                    ans.append(num)
+        
+        num = 0
+        start_digit = int(low/pow(10,digits_low-1))
+        pos = start_digit-1
+        digit = digits_low
+        
+        while num <= high:
+            if num>=low:
+                ans.append(num)
+            
+            if int(superset[pos:pos+digit]) == num:
+                break
+                
+            num = int(superset[pos:pos+digit])
+                
+            pos += 1
+            
+            if num%10==9:
+                pos = 0
+                digit += 1
+            
 
         return ans
         
