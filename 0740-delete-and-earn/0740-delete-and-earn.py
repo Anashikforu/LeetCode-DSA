da83675deadb45316fa7381bdf04fa4e679e5731
Ashik Khan <aasshhik98@gmail.com>
2024-09-08 01:39:28 +0600

    Time: 77 ms (47.96%), Space: 18.3 MB (96.84%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -3,14 +3,16 @@ class Solution:
         
         summation = [0]*10002
         
+        maxi = max(nums)
+        
         for num in nums:
             summation[num] += num
             
-        for i in range(2,10002):
+        for i in range(2,maxi+1):
             summation[i] = max(summation[i]+summation[i-2],summation[i-1])
         
         
-        return summation[10001]
+        return summation[maxi]
         
         
         
