ba639da12c0e4e7b9c0f54ff356019777dd4c519
Ashik Khan <aasshhik98@gmail.com>
2024-09-11 07:40:00 +0600

    Time: 39 ms (28.71%), Space: 16.6 MB (26.96%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,11 +1,26 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        res = 0
-        xor = start ^ goal
         
-        while xor:
-            xor &= (xor -1)
-            res += 1
+        startB = str(format(int(start), 'b'))
+        goalB = str(format(int(goal), 'b'))
+        res = 0
+        
+        gap = abs(len(startB) - len(goalB))
+        
+        n = max(len(goalB),len(startB))
+        
+        greater , lesser = goalB , startB
+        
+        if len(goalB) < len(startB):
+            greater , lesser = startB , goalB
+            
+        for i in range(gap):
+            if greater[i] != '0':
+                res += 1
+        
+        for i in range(n-gap):
+            if greater[i+gap] != lesser[i]:
+                res += 1
         
         return res
         
